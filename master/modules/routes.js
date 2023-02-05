eb0149bff9c4a88d7fe64ccb865dac5492ee0fd8
uxodopiva <peter.ovseychuk@gmail.com>
2023-02-06 00:46:30 +0200

    pinger: logs request: filter unnecessary logs

--- master/modules/routes.js
+++ master/modules/routes.js
@@ -7,7 +7,10 @@ const normalizeLogs = (logs) => {
         const {label, status, timestamp} = log;
         const slave = result.find(slave => slave.label === label);
         if (slave) {
-            if (slave.logs[slave.logs.length - 1].status === status) continue;
+            if (slave.logs[slave.logs.length - 1].status === status){
+                slave.logs[slave.logs.length - 1].timestamp = timestamp;
+                continue;
+            }
             slave.logs.push({status, timestamp});
         } else {
             result.push({
